9550beb2c34d7a38120d00db8645150a3a1acd50
m83city <rostorguievv@gmail.com>
2024-06-29 06:47:04 +0300

    first commit

---
+++ src/component/Header.tsx
@@ -0,0 +1,42 @@
+import facebook from "../img/facebook.svg";
+import logo from "../img/logo.svg";
+import twitter from "../img/twitter.svg";
+import instagram from "../img/instagram.svg";
+import { v4 as uuidv4 } from "uuid";
+import "./Header.scss";
+function Header() {
+  const headerMenu: string[] = [
+    "Home",
+    "Recipes",
+    "Blog",
+    "Contact",
+    "About us",
+  ];
+  const headerSocialMediaGroup: string[] = [facebook, instagram, twitter];
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <img className="headerLogo" src={logo} alt="logo"></img>
+        <div className="headerMenuContainer">
+          {headerMenu.map((element) => (
+            <div className="headerMenuElement" key={uuidv4()}>
+              {element}
+            </div>
+          ))}
+        </div>
+        <div className="headerSocialMediaContainer">
+          {headerSocialMediaGroup.map((element) => (
+            <img
+              className="headerSocialMediaElement"
+              key={uuidv4()}
+              src={element}
+              alt="some"
+            ></img>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/home/Home.tsx
@@ -0,0 +1,10 @@
+import Header from "../component/Header";
+
+function Home() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+export default Home;
